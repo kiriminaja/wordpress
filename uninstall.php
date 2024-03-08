c942968b77ec10b14a747c9d44714986ae8aefbe
FaizRA <faizniflehim@gmail.com>
2024-03-08 10:11:50 +0700

    update

--- uninstall.php
+++ uninstall.php
@@ -12,12 +12,12 @@ if ( ! defined('WP_UNINSTALL_PLUGIN')){ die; }
 // Clear database storage data
 
 global $wpdb;
-$table_kiriminaja_transactions = $wpdb->prefix.'kiriminaja_transactions';
-$table_kiriminaja_settings = $wpdb->prefix.'kiriminaja_settings';
-$table_kiriminaja_transactions = $wpdb->prefix.'kiriminaja_transactions';
+$table_kiriminaja_transactions  = $wpdb->prefix.'kiriminaja_transactions';
+$table_kiriminaja_settings      = $wpdb->prefix.'kiriminaja_settings';
+$table_kiriminaja_payments      = $wpdb->prefix.'kiriminaja_payments';
 
 $wpdb->query("DROP TABLE IF EXISTS $table_kiriminaja_transactions");
 $wpdb->query("DROP TABLE IF EXISTS $table_kiriminaja_settings");
-$wpdb->query("DROP TABLE IF EXISTS $table_kiriminaja_transactions");
+$wpdb->query("DROP TABLE IF EXISTS $table_kiriminaja_payments");
 delete_option("my_plugin_db_version");
 
