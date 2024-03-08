a5ea4e5aaab6e36e151c80759231eedc0ebc2af0
FaizRA <faizniflehim@gmail.com>
2024-03-08 09:39:25 +0700

    AB#4567 - hide menu if woocommerce isnt installed

--- templates/request-pickup/index.php
+++ templates/request-pickup/index.php
@@ -3,13 +3,6 @@
 
 class requestPickupIndex {
     function __construct(){
-        
-        /** Redirect jika plugin woocommerce blm terinstall*/
-        if (!KJ_CHECK_WOOCOMMERCE()){
-            header("location: ".home_url().'/wp-admin/admin.php?page=kiriminaja-konfigurasi');
-            exit();
-        }
-        
         global $results;
         global $page;
         global $items_per_page;
