1a342d8229b09ee14513e3339cd142baa1720333
FaizRA <faizniflehim@gmail.com>
2024-03-15 16:34:22 +0700

    update

--- inc/Base/BaseInit.php
+++ inc/Base/BaseInit.php
@@ -12,7 +12,7 @@ class BaseInit{
         $this->plugin_path =  plugin_dir_path(dirname(__FILE__,2));
         $this->plugin_url = plugin_dir_url(dirname(__FILE__,2));
         $plugin = plugin_basename( dirname( __FILE__, 3 ) );
-        $this->plugin = $plugin . "/" . 'kiriminaja-plugin.php' ;
+        $this->plugin = $plugin . "/" . 'kiriminaja.php' ;
     }
 
     public function logThis($test='log',$loggedItem=[]){
