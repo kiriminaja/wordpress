fc8563d6e30e9dbd0e21ec97c030926aad4e53df
FaizRA <faizniflehim@gmail.com>
2024-04-19 14:33:16 +0700

    check adj

--- inc/Services/PluginInfoServices/UpdateCheckService.php
+++ inc/Services/PluginInfoServices/UpdateCheckService.php
@@ -12,7 +12,7 @@ class UpdateCheckService extends BaseService{
         try {
             $response = self::callInfoJson();
 
-            $latestVersion = @json_decode($response)->version ?? '0.0.0';
+            $latestVersion = str_replace('pre-release-v','',(@json_decode($response)->version ?? '0.0.0')) ;
             $currentVersion = @thePluginData()['Version'] ?? '0.0.0';
 
             $versionInArray = [$latestVersion, $currentVersion];
@@ -36,7 +36,7 @@ class UpdateCheckService extends BaseService{
     
     protected function callInfoJson(){
         $curl_handle=curl_init();
-        curl_setopt($curl_handle, CURLOPT_URL,'https://analytics.kiriminaja.com/wooooo/woocommerce.json');
+        curl_setopt($curl_handle, CURLOPT_URL,'https://storage.googleapis.com/tprt0ezsggqjornc7nf1wwluvgulhr/wp/woocommerce.json');
         curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
         curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
