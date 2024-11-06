479b51ce29ba08abb4500d2734304116fa9c70b1
masikhsanms <masikhsanmarda@gmail.com>
2024-11-06 14:36:53 +0700

    Remove Base Url Dev

--- inc/Base/KiriminAjaApi.php
+++ inc/Base/KiriminAjaApi.php
@@ -11,7 +11,6 @@ class KiriminAjaApi
     {
         global $wp_version;
         $this->base_url = 'https://client.kiriminaja.com'; // PRODUCTION
-        // $this->base_url = 'https://dev-core.bakso.my.id'; // DEV
         
         $dbApiToken = (new \Inc\Repositories\SettingRepository())->getSettingByKey('api_key')->value ?? '';
         
