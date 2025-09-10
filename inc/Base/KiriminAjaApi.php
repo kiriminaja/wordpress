26e085b84084be72cfc5358557e9f5ccd85dd4b0
Anggi Susanto <antscpk06@gmail.com>
2025-09-10 13:50:10 +0700

    fix: update base URL to development environment in KiriminAjaApi class

--- inc/Base/KiriminAjaApi.php
+++ inc/Base/KiriminAjaApi.php
@@ -10,7 +10,7 @@ class KiriminAjaApi
     public function __construct()
     {
         global $wp_version;
-        $this->base_url = 'https://dev-core.bakso.my.id'; // PRODUCTION
+        $this->base_url = 'https://dev-kaj-mitra-srvc-ext.bakso.my.id'; // DEVELOPMENT
         
         $dbApiToken = (new \Inc\Repositories\SettingRepository())->getSettingByKey('api_key')->value ?? '';
         
