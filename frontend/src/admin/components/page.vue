c381e110c7e064b13086d66ae7f36dc6c18bd142
Yanuar Aditia <yanuaraditia@outlook.com>
2025-12-02 15:57:35 +0700

    AB#25858 feat: add navbar update

--- frontend/src/admin/components/page.vue
+++ frontend/src/admin/components/page.vue
@@ -1,12 +1,34 @@
 <template>
-  <UDashboardNavbar>
-    <template #left>
-      <UBreadcrumb :items="items" />
-    </template>
-    <template #right>
-      <slot name="actions" />
-    </template>
-  </UDashboardNavbar>
+  <div class="sticky top-0 bg-white p-3 flex justify-between gap-3">
+    <ul class="m-0! p-0! flex items-center">
+      <li class="m-0">
+        <img src="https://kiriminaja.com/favicon.ico" class="w-6 h-6" alt="" />
+      </li>
+      <template v-for="(value, key) in items" :key="value.label">
+        <li
+          :class="{
+            'text-base m-0': true,
+            'font-semibold': key === items.length - 1,
+          }"
+        >
+          <a v-if="value.to" :href="value.to" class="text-inherit! opacity-80">
+            {{ value.label }}
+          </a>
+          <span v-else>
+            {{ value.label }}
+          </span>
+        </li>
+        <!-- show separator if not last -->
+        <li
+          v-if="key < items.length - 1"
+          class="inline-block mx-2 text-gray-400 m-0"
+        >
+          <UIcon name="lucide:chevron-right" class="size-4" />
+        </li>
+      </template>
+    </ul>
+    <slot name="actions" />
+  </div>
   <div class="p-3">
     <slot />
     <div class="text-center mt-3">
