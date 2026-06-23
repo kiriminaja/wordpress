16aa13bd5738810b5ab0a9319ae5384cecf17770
Yanuar <me@yan.ad>
2026-06-23 09:27:43 +0700

    chore: bump minimum WordPress version to 6.8

--- kiriminaja.php
+++ kiriminaja.php
@@ -11,7 +11,7 @@
  * Text Domain:     kiriminaja-official
  * Domain Path:     /lang
  * Requires Plugins: woocommerce
- * Requires at least: 6.0
+ * Requires at least: 6.8
  * Requires PHP: 8.0
  * WC requires at least: 8.0.0
  * WC tested up to: 10.6
